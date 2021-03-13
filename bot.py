d1adc03fab55eb4e728ce273d155669e2732e02d
xeph <tronical@gmail.com>
2021-03-12 19:21:00 -0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,45 @@
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
+from twitter import Twitter
+import discord
+import re
+import os
+
+client = discord.Client()
+
+tweet_id_pattern = re.compile(r"https?://[www\.]?twitter\.com/[^ ]+/status/(\d+)")
+api = Twitter()
+
+def get_emoji(digit):
+    return str(digit) + u'\ufe0f\u20e3'
+
+def get_emojis(number):
+    return list(map(get_emoji, str(number)))
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    tweet_ids = tweet_id_pattern.findall(message.content)
+
+    if len(tweet_ids) == 0:
+        return
+
+    num_pics = api.count_pics(tweet_ids)
+
+    if num_pics == 0:
+        return
+
+    reactions = get_emojis(num_pics)
+
+    for reaction in reactions:
+        print(f"Adding reaction {reaction}")
+        await message.add_reaction(reaction)
+
+client.run(os.environ['DISCORD_TOKEN'])
---
+++ twitter.py
@@ -0,0 +1,21 @@
+import os
+import requests
+
+class Twitter:
+    def __init__(self):
+        self.KEY = os.environ['TWITTER_API_KEY']
+        self.SECRET = os.environ['TWITTER_API_SECRET']
+        self.BEARER = os.environ['TWITTER_API_BEARER']
+
+    def request(self, tweet_ids):
+        headers = {"Authorization": f"Bearer {self.BEARER}"}
+        url = f"https://api.twitter.com/2/tweets?ids={','.join(tweet_ids)}&media.fields=type&expansions=attachments.media_keys"
+        return requests.get(url, headers=headers).json()
+
+    def count_pics(self, tweet_ids):
+        response = self.request(tweet_ids)
+
+        if 'includes' not in response:
+            return 0
+
+        return len(response['includes']['media'])
